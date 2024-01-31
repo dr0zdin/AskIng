4bf81df3ae9a003f0ed9fed56b405b851216d7ec
Hubert Drozdowski <hubert.drozdowski@pcmicorp.com>
2024-01-31 20:50:31 +0100

    Implement JWT

--- AskIng.API/Program.cs
+++ AskIng.API/Program.cs
@@ -1,11 +1,24 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+    .AddDefaultTokenProviders();
 builder.Services.AddSwaggerGen();
+
+ConfigureJwt(builder.Services);
 
 var app = builder.Build();
 
@@ -23,3 +36,29 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void ConfigureJwt(IServiceCollection services)
+{
+    SecretClient keyValueClinet = new SecretClient(
+        new Uri(config.GetValue<string>("KeyVaultUri")),
+        new DefaultAzureCredential());
+
+    services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "should.be.taken.from.outside.issuer",
+            ValidAudience = "should.be.taken.from.outside.audience",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("should.be.taken.from.outside.secret_key"))
+        };
+    });
+}
